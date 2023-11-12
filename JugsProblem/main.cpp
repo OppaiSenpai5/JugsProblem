f388aa4cf3870b3012badcd2e936e846ade26f95
Todor Petrov <todorpetrov55@gmail.com>
2023-11-12 15:57:18 +0200

    Added input validation

--- JugsProblem/main.cpp
+++ JugsProblem/main.cpp
@@ -1,21 +1,52 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
 #include "JugsProblem.h"
+
+const char* INVALID_INPUT_MESSAGE = "The input supplied is invalid";
+
+JugsProblem::liters read_input()
+{
+	std::string input;
+	std::getline(std::cin, input);
+	bool valid_input = !input.empty() &&
+		std::find_if(input.cbegin(), input.cend(),
+			[](char c) { return !std::isdigit(c); }) == input.cend();
+
+	if (!valid_input)
+	{
+		throw INVALID_INPUT_MESSAGE;
+	}
+	std::istringstream number(input);
+	JugsProblem::liters result;
+	number >> result;
+
+	return result;
+}
 
 int main()
 {
-	size_t x, y, z, v;
 
-	std::cout << "x=";
-	std::cin >> x;
-	std::cout << "y=";
-	std::cin >> y;
-	std::cout << "z=";
-	std::cin >> z;
-	std::cout << "v=";
-	std::cin >> v;
+	try
+	{
+		std::cout << "x=";
+		JugsProblem::liters x = read_input();
+		std::cout << "y=";
+		JugsProblem::liters y = read_input();
+		std::cout << "z=";
+		JugsProblem::liters z = read_input();
+		std::cout << "v=";
+		JugsProblem::liters v = read_input();
 
-	JugsProblem jugs_problem(x, y, z, v);
-	jugs_problem.solve();
+		JugsProblem jugs_problem(x, y, z, v);
+		jugs_problem.solve();
+	}
+	catch (const char* exception)
+	{
+		std::cout << exception << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
